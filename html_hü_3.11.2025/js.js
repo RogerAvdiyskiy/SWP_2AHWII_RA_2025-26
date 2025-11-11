b17e5944b6dc65178fdc5f3aa34b0af6d3cc515b
Roger Avdiyskiy <AVD240096@spengergasse.at>
2025-11-11 21:13:39 +0100

    gestylt wie ein löwe

--- html_hü_3.11.2025/js.js
+++ html_hü_3.11.2025/js.js
@@ -12,16 +12,25 @@ const tierBild = document.getElementById('tier_bild');
 selectTier.addEventListener('change', () => {
     const wert = selectTier.value;
     if (wert) {
-        anzeige.textContent = `Du hast "${wert}" ausgewählt!`;
+        //anzeige.textContent = `Du hast "${wert}" ausgewählt!`;
         if (wert === "Affe") {
             tierBild.src = "affe.jpeg";
             tierBild.alt = "Bild eines Affen";
+            anzeige.textContent = "Affenstark!"
+            anzeige.style.border = "3px solid rgb(132, 64, 64)";
+            anzeige.style.backgroundColor = "rgb(132, 64, 64)";
         } else if (wert === "Chameleon") {
             tierBild.src = "chameleon.jpeg";
             tierBild.alt = "Bild eines Chamäleons";
+            anzeige.textContent = "Abwechslungsreich!"
+            anzeige.style.border = "3px solid lightgreen"
+            anzeige.style.backgroundColor = "lightgreen";
         } else if (wert === "Stinkekäfer") {
             tierBild.src = "stinkekäfer.jpeg";
             tierBild.alt = "Bild eines Stinkekäfers";
+            anzeige.textContent = "Das würd mir stinken!"
+            anzeige.style.border = "3px solid lightyellow";
+            anzeige.style.backgroundColor = "lightyellow";
         }
     } else {
         anzeige.textContent = '';
